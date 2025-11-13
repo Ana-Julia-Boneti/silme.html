d05df9e8a86e2077fe7670e1ff86bb26de47c4de
Ana Julia Boneti Volgue <ana.volgue@escola.pr.gov.br>
2025-11-13 19:38:15 +0000

    slime pronta

---
+++ script.js
@@ -0,0 +1,23 @@
+function cola(){
+    document.getElementById("slime").src="cola.jpg"
+    console.log("cola")
+}
+
+function ativador(){
+    document.getElementById("slime").src="ativador.jpg"
+     console.log("ativador")
+}
+
+function corante(){
+    document.getElementById("slime").src="corante.jpeg"
+     console.log("corante")
+}
+
+function misturar(){
+    document.getElementById("slime").src="mistura.jpg"
+     console.log("misturar")
+}
+function pronta(){
+    document.getElementById("slime").src="pronta.jpg"
+     console.log("pronta")
+}
